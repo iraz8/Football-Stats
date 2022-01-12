c5d2521ef80ed0131b1309e31ada104c73c136e6
Razvan <razvaniulianb@outlook.com>
2022-01-12 21:41:11 +0200

    Initial commit

---
+++ src/main/java/com/football/football_stats/FootballStatsApplication.java
@@ -0,0 +1,13 @@
+package com.football.football_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballStatsApplication.class, args);
+    }
+
+}
